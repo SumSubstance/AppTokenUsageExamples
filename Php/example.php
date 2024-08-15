6a260977234476a8148b73455aaf7246950c7682
rozenws <68683462+rozenws@users.noreply.github.com>
2024-08-15 15:08:08 +0300

    Update example.php

--- Php/example.php
+++ Php/example.php
@@ -5,12 +5,12 @@ use Sumsub\AppTokenUsageExample\SumsubClient;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-// The description of the authorization method is available here: https://developers.sumsub.com/api-reference/#app-tokens
+// The description of the authorization method is available here: https://docs.sumsub.com/reference/authentication
 // Don't forget to change token and secret key values to production ones when switching to production
 const SUMSUB_SECRET_KEY = 'SUMSUB_SECRET_KEY'; // Example: Hej2ch71kG2kTd1iIUDZFNsO5C1lh5Gq
 const SUMSUB_APP_TOKEN = 'SUMSUB_APP_TOKEN'; // Example: sbx:uY0CgwELmgUAEyl4hNWxLngb.0WSeQeiYny4WEqmAALEAiK2qTC96fBad
 
-// The description of the flow can be found here: https://developers.sumsub.com/api-flow/#api-integration-phases
+// The description of the flow can be found here: https://docs.sumsub.com/reference/get-started-with-api
 
 // Such actions are presented below:
 // 1) Creating an applicant
